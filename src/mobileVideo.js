5387dce53b1f82d57ef7ca7a621f9a405d6948f6
netease_wb1 <495567615@qq.com>
2017-07-19 11:47:28 +0800

    a

--- src/mobileVideo.js
+++ src/mobileVideo.js
@@ -2,7 +2,8 @@ var mobileVideoPlayer = function(){ //定义单例
 
     var _class = function(options){//内部类
         this.options=$.extend({
-            playBtn:''
+            playBtn:'',
+            src:''
         },options);
         this.init();
     }
@@ -16,15 +17,23 @@ var mobileVideoPlayer = function(){ //定义单例
         addEvent:function(){//事件绑定
             console.log(3333)
         },
-        setTamplate:function(vid){
+        setTamplate:function(){
             var self = this;
-            self.tamplate = '<video src="'+vid+'" controls="controls" id="tsvideo'+self.timeNumId+'"></video><div class="bg'+self.timeNumId+'"></div>';
-            $('body').append('<div id="video'+self.timeNumId+'"></div>');
+            if(!self.tamplate){
+                var vid = self.options.src;
+                self.tamplate = '<div class="bg'+self.timeNumId+'"></div><video src="'+vid+'" controls="controls" id="tsvideo'+self.timeNumId+'"></video><div class="vid-close'+self.timeNumId+'"><i></i><em></em></div>';
+            }
+            if($('#video'+self.timeNumId).length<1){
+                $('body').append('<div id="video'+self.timeNumId+'"></div>');
+            }
             return self.tamplate;
         },
         addDomClass:function(){
             var self = this;
             var cssText = "#video"+self.timeNumId+"{position:fixed;width:100%;height:100%;top:0;left:0;display:none;z-index:9999;bottom:0} "
+            + "#video"+self.timeNumId+" .vid-close"+self.timeNumId+"{position: absolute;width: 44px;height: 50px;top: 100px;left: 50%;-webkit-transform: translate3d(-50%,-50%,0);transform: translate3d(-50%,-50%,0);display: block;z-index: 11000;} "
+            + "#video"+self.timeNumId+" .vid-close"+self.timeNumId+" i{position: absolute;top:4px;left:2px;display: block;width: 5px;height: 50px;background: #fff;-webkit-transform: rotate(-45deg);-webkit-transform-origin: 0 0 0;transform: rotate(-45deg);transform-origin: 0 0 0;}"
+            + "#video"+self.timeNumId+" .vid-close"+self.timeNumId+" em{position: absolute;top:0;right:0;display: block;width: 5px;height: 50px;background: #fff;-webkit-transform: rotate(45deg);-webkit-transform-origin: 0 0 0;transform: rotate(45deg);transform-origin: 0 0 0;}"
             + "#video"+self.timeNumId+" .bg"+self.timeNumId+"{position:absolute;top:0;left:0;width:100%;height:100%;background:#000;opacity:.8;bottom:0} "
             + "#tsvideo"+self.timeNumId+"{position:absolute;top:50%;left:0;width:100%;-webkit-transform: translate3d(0,-50%,0);transform: translate3d(0,-50%,0);}";
             $('body').append("<style type='text/css'>"+cssText+"</style>");
@@ -32,15 +41,15 @@ var mobileVideoPlayer = function(){ //定义单例
         setVideo:function(){
             var self = this;
             self.playBtn = self.options.playBtn;
-            var _child = '.bg'+self.timeNumId;
+            var _child = '.bg'+self.timeNumId + ', .vid-close'+self.timeNumId;
             $("#video"+self.timeNumId).on("touchend",_child, function () {
                setTimeout(function () {
                    $("#video"+self.timeNumId).hide(), $("#video"+self.timeNumId).html("");
                 }, 100)
             });
             $(self.playBtn).on('click',function () {
-                var e = $(this).attr("data-vid");
-                $("#video"+self.timeNumId).html(self.setTamplate(e)).show();
+                // var e = $(this).attr("data-vid");
+                $("#video"+self.timeNumId).html(self.setTamplate()).show();
                 var n = $("#tsvideo"+self.timeNumId).get(0);
                 setTimeout(function () {
                     n.play();
@@ -52,7 +61,7 @@ var mobileVideoPlayer = function(){ //定义单例
             var self = this;
             self.timeNumId = Math.floor(Math.random()*100);
             self.addDomClass();
-            // self.setTamplate();
+            self.setTamplate();
             self.setVideo();
         }
     }
